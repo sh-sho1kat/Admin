587cbbb9e8eb400b21245295ed561ee2b4ba6364
sh-sho1kat <shefat.hossen.045@gmail.com>
2023-09-12 12:48:18 +0600

    Commit 5

---
+++ app/src/main/java/com/example/admin/userpart/about/About_Activity.java
@@ -0,0 +1,14 @@
+package com.example.admin.userpart.about;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.admin.R;
+
+public class About_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
